5b31474c2a94087958a5b6dd0057031826faa47a
Gonzalo Esquivel <gesquivel263@gmail.com>
2023-12-20 12:56:47 -0300

    Operadores

---
+++ Clase_01.py
@@ -0,0 +1,22 @@
+numero1 = 7
+numero2 = 5
+suma = numero1+numero2
+print(suma)
+
+numero3 = "7"
+numero4 = "5"
+suma = numero3+numero4
+print(suma)
+
+dato = 1
+print(type(dato))
+dato = "1 esto es un string"
+print(type(dato))
+dato = 1.0
+print(type(dato))
+dato = True
+print(type(dato))
+dato = 3 + 5j
+print(type(dato))
+
+
---
+++ ejercicio_01.py
@@ -0,0 +1,19 @@
+numero1= 8
+numero2= 6
+suma = numero1 + numero2
+print(suma)
+
+numero3= 8
+numero4= 6
+resta = numero3 - numero4
+print(resta)
+
+numero5= 8
+numero6= 6
+multiplicacion = numero5 * numero6
+print(multiplicacion)
+
+numero7= 8
+numero8= 6
+division = numero7 + numero8
+print(division)
